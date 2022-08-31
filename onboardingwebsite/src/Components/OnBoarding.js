52d4de46705e8dbc991429795b7eeba88c882975
Bhaskar <greddy@pipingrock.com>
2022-08-31 11:29:39 +0530

    Html Css Practice

---
+++ onboardingwebsite/src/Components/OnBoarding.js
@@ -0,0 +1,8 @@
+import React, {useState, useEffect } from "react";
+import Button from "@mui/material/Button"
+export default function OnBoarding(){
+    console.log('In component onBoarding')
+    return(
+        <Button variant="contained" sx={{mt:10}}>TestButton</Button>
+    );
+}
