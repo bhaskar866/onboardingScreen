e37ca94e6e2ff7fa6e6ceb67c5301ba28d313d68
Bhaskar <greddy@pipingrock.com>
2022-08-31 11:43:40 +0530

    Route Changes

---
+++ onboardingwebsite/src/Components/OnBoarding.js
@@ -0,0 +1,7 @@
+import React, {useState, useEffect } from "react";
+import Button from "@mui/material/Button"
+export default function OnBoarding(){
+    return (
+<Button variant="contained" sx={{mt:10}}>TestButton</Button>
+    );
+}
